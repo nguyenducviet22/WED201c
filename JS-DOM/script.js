4d14ed15585588906341d5a58dcc1c86077f6cf2
nguyenducviet22 <ndv6060@gmail.com>
2024-08-11 17:24:42 +0700

    update 11/8

---
+++ JS-DOM/script.js
@@ -0,0 +1,35 @@
+// Basic JS commands
+// let a = 4;
+// let b = 5;
+// let c = a + b;
+// console.log(c);
+// alert("Hello")
+// confirm("Are you sure")
+// let a = prompt("Enter your name: ")
+// console.log(a)
+
+// DOM Traversal
+// const hds = document.getElementsByClassName('heading');
+// console.log(hds);
+// const hd1 = document.getElementById('h1');
+// console.log(hd1);
+// const hd2 = document.querySelector('.heading');//for class
+// console.log(hd2);
+// const hd3 = document.querySelector('#h3');//for id
+// console.log(hd3);
+
+// Add and Edit Elements
+const body = document.querySelector('body');
+body.append('Hello World');
+const newDiv = document.createElement('div');
+const bold = document.createElement('b');
+bold.innerText = 'Hello World';
+newDiv.append(bold);
+body.append(newDiv);
+const heading1 = document.getElementById('h1');
+heading1.style.color = 'red';
+body.style.backgroundColor = 'gray'
+
+//Delete Elements
+const heading2 = document.getElementById('h2');
+heading2.remove();
