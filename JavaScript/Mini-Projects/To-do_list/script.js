78667a17faab3e39827cd33bf6c5a09ab97ffac7
nguyenducviet22 <ndv6060@gmail.com>
2024-08-30 21:02:00 +0700

    update 30/8

--- JavaScript/Mini-Projects/To-do_list/script.js
+++ JavaScript/Mini-Projects/To-do_list/script.js
@@ -1,9 +1,16 @@
+let list = ['DBI', 'CSD'];
+let contentList = document.getElementById('content-list');
+for (let i = 0; i < list.length; i++){
+    let li = document.createElement('li');
+    li.innerHTML = list[i];
+    contentList.appendChild(li);
+}
+
 function add(){
     let input = document.getElementById('input');
     let text = input.value;
     let li = document.createElement('li');
     li.innerHTML = text;
-    let list = document.getElementById('content-list');
     list.appendChild(li);
     input.value = "";
 }
